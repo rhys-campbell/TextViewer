4180cbc0fca48f2132d3a019264cb55c6c74d9a1
Rhys Campbell <rhys.campbell1@my.jcu.edu.au>
2015-03-09 10:23:50 +1000

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jc278710 on 9/03/15.
+ */
+public class App {
+    public static void main(String[] args) {
+        String text = "This is a simple test\nOne Two Three\nHello world!";
+        for (int i = 0; i < text.length(); ++i) {
+            System.out.println(text.charAt(i));
+        }
+        System.out.println();
+    }
+}
